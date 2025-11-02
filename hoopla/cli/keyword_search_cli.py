a902a3b924956d68f0597cdcc80d735d840ba931
mdmdj <17350097+mdmdj@users.noreply.github.com>
2025-11-02 13:29:30 +0700

    CH2 L3

--- hoopla/cli/keyword_search_cli.py
+++ hoopla/cli/keyword_search_cli.py
@@ -4,85 +4,39 @@ import argparse
 import json
 import string
 from nltk.stem import PorterStemmer
+from lib.keyword_search import build_command, search_command
+
+
+DEFAULT_SEARCH_LIMIT = 50
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
-    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    subparsers = parser.add_subparsers(
+        dest="command", help="Available commands")
 
-    search_parser = subparsers.add_parser("search", help="Search movies using BM25")
+    search_parser = subparsers.add_parser(
+        "search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
+
+    build_parser = subparsers.add_parser("build", help="Build index")
 
     args = parser.parse_args()
 
-    # loading movie data
-    movieDataFile = open("./data/movies.json", "r")
-    movieDataJson = json.load(movieDataFile)
-    movieDataFile.close()
-
-    #print(movieDataJson)
-
-    # loading stop words
-    stopWordsFile = open("./data/stopwords.txt", "r")
-    stopWords = stopWordsFile.read().splitlines()
-    stopWordsFile.close()
-
     match args.command:
-      case "search":
-          query = args.query
+        case "build":
+            print("Building index...")
+            build_command()
+            print("Index built successfully.")
 
-          print(f"Searching for: {query}")
+        case "search":
+            print("Searching for:", args.query)
+            results = search_command(args.query)
+            for r in (results):
+                print(f"{r["id"]} {r["title"]}")
 
-          # remove string.punctuation from query
-          query = query.translate(str.maketrans('', '', string.punctuation))
-          print(f"{query}")
-
-          # lowercase query
-          query = query.lower()
-          print(f"{query}")
-
-          # split on whitespace
-          queryWords = query.split()
-          print(f"{queryWords}")
-
-          # remove stop words from query
-          queryWords = [word for word in queryWords if word not in stopWords]
-          print(f"{queryWords}")
-
-          # filter query to stems
-          stemmer = PorterStemmer()
-          queryWords = [stemmer.stem(word) for word in queryWords]
-          print(f"{queryWords}")
-
-          results = []
-
-          for movie in movieDataJson["movies"]:
-              titleToMatch = movie["title"].lower().translate(str.maketrans('', '', string.punctuation))
-              titleSplit = titleToMatch.split()
-              # remove stop words from title
-              titleSplit = [word for word in titleSplit if word not in stopWords]
-              # fileter title to stems
-              titleSplit = [stemmer.stem(word) for word in titleSplit]
-              if any(
-                  q in t
-                  for q in queryWords
-                  for t in titleSplit
-              ):
-                  results.append(movie)
-
-          print(f"Found {len(results)} results")
-
-          # order results by id
-          results = sorted(results, key=lambda x: x["id"])
-
-          # slice off top 5 results
-          results = results[:5]
-
-          # print the results
-          for result in results:
-              print(f"{result['title']} - {result['id']}")
-
-      case _:
-          parser.print_help()
+        case _:
+            parser.exit(2, parser.format_help())
 
 
 if __name__ == "__main__":
